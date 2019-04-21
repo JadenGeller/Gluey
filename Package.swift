fb926e9584813f848890499c147e4c0f0e346489
Max Desiatov <max@desiatov.com>
2019-04-21 22:27:58 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
-
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to
+// build this package.
 import PackageDescription
 
 let package = Package(
